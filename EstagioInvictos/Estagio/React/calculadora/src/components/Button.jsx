94679ec63f2c8525366de2f2dcce0f2a33eaba2c
Leonardo yuki <134506662+LeonardoTsuyoshiYuki@users.noreply.github.com>
2023-06-21 17:17:05 -0400

    21062023(Calculadora)

--- EstagioInvictos/Estagio/React/calculadora/src/components/Button.jsx
+++ EstagioInvictos/Estagio/React/calculadora/src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './Button.css'
-export default props =>{
 
-    let classes = 'button'
-        classes += props.operation ? 'operation' : ''
-        classes += props.double ? 'double' : ''
-        classes += props.triple ? 'triple' : ''
+
+export default props =>{
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
 
     return (
         <button
@@ -15,3 +16,4 @@ export default props =>{
          </button> 
     )
 }
+
